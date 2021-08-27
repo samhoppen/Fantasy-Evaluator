bbf809f180ff8bb4f472f5011f43ba62edb8b5d9
samhoppen <sphoppen@gmail.com>
2021-08-27 14:55:13 -0500

    Add FL logo

--- Code/Image themes.R
+++ Code/Image themes.R
@@ -45,7 +45,7 @@ logo_asp <- 1.618
 
 showtext_opts(dpi = 480)
 
-brand_nfl_plot <- function(orig_plot, save_name, asp = 16/9, base_size = 5, tm_wordmarks = F, logo = F,
+brand_nfl_plot <- function(orig_plot, save_name, asp = 16/9, base_size = 5, tm_wordmarks = F, logo = F, logo_FL = F,
                                logo_ETR = F, logo_FE = F, logo_4for4 = F, logo_4for4_red = F, logo_SC = F, logo_loc) {
   
   ## start by adding team wordmarks
@@ -99,6 +99,13 @@ brand_nfl_plot <- function(orig_plot, save_name, asp = 16/9, base_size = 5, tm_w
   }
   if (logo_4for4_red){
     logo_file <- magick::image_read("C:/Users/Hoppy/OneDrive/NFL Analysis/Data Repository/4for4_red.jpg")
+    logo_width <- 0.09
+    logo_height <- 0.09
+    logo_x <- 0.925
+    logo_y <- 0.91
+  }
+  if (logo_FL){
+    logo_file <- magick::image_read("C:/Users/Hoppy/OneDrive/NFL Analysis/Data Repository/FL_logo.jpg")
     logo_width <- 0.09
     logo_height <- 0.09
     logo_x <- 0.925
@@ -365,74 +372,74 @@ theme_FE <-  theme(
 # )
 
 ##### CUSTOM FACET WRAP HELPER FUNCTIONS ####
-scale_override <- function(which, scale) {
-  if(!is.numeric(which) || (length(which) != 1) || (which %% 1 != 0)) {
-    stop("which must be an integer of length 1")
-  }
-  
-  if(is.null(scale$aesthetics) || !any(c("x", "y") %in% scale$aesthetics)) {
-    stop("scale must be an x or y position scale")
-  }
-  
-  structure(list(which = which, scale = scale), class = "scale_override")
-}
-
-
-CustomFacetWrap <- ggproto(
-  "CustomFacetWrap", FacetWrap,
-  init_scales = function(self, layout, x_scale = NULL, y_scale = NULL, params) {
-    # make the initial x, y scales list
-    scales <- ggproto_parent(FacetWrap, self)$init_scales(layout, x_scale, y_scale, params)
-    
-    if(is.null(params$scale_overrides)) return(scales)
-    
-    max_scale_x <- length(scales$x)
-    max_scale_y <- length(scales$y)
-    
-    # ... do some modification of the scales$x and scales$y here based on params$scale_overrides
-    for(scale_override in params$scale_overrides) {
-      which <- scale_override$which
-      scale <- scale_override$scale
-      
-      if("x" %in% scale$aesthetics) {
-        if(!is.null(scales$x)) {
-          if(which < 0 || which > max_scale_x) stop("Invalid index of x scale: ", which)
-          scales$x[[which]] <- scale$clone()
-        }
-      } else if("y" %in% scale$aesthetics) {
-        if(!is.null(scales$y)) {
-          if(which < 0 || which > max_scale_y) stop("Invalid index of y scale: ", which)
-          scales$y[[which]] <- scale$clone()
-        }
-      } else {
-        stop("Invalid scale")
-      }
-    }
-    
-    # return scales
-    scales
-  }
-)
-
-facet_wrap_custom <- function(..., scale_overrides = NULL) {
-  # take advantage of the sanitizing that happens in facet_wrap
-  facet_super <- facet_wrap(...)
-  
-  # sanitize scale overrides
-  if(inherits(scale_overrides, "scale_override")) {
-    scale_overrides <- list(scale_overrides)
-  } else if(!is.list(scale_overrides) || 
-            !all(vapply(scale_overrides, inherits, "scale_override", FUN.VALUE = logical(1)))) {
-    stop("scale_overrides must be a scale_override object or a list of scale_override objects")
-  }
-  
-  facet_super$params$scale_overrides <- scale_overrides
-  
-  ggproto(NULL, CustomFacetWrap,
-          shrink = facet_super$shrink,
-          params = facet_super$params
-  )
-}
+# scale_override <- function(which, scale) {
+#   if(!is.numeric(which) || (length(which) != 1) || (which %% 1 != 0)) {
+#     stop("which must be an integer of length 1")
+#   }
+#   
+#   if(is.null(scale$aesthetics) || !any(c("x", "y") %in% scale$aesthetics)) {
+#     stop("scale must be an x or y position scale")
+#   }
+#   
+#   structure(list(which = which, scale = scale), class = "scale_override")
+# }
+# 
+# 
+# CustomFacetWrap <- ggproto(
+#   "CustomFacetWrap", FacetWrap,
+#   init_scales = function(self, layout, x_scale = NULL, y_scale = NULL, params) {
+#     # make the initial x, y scales list
+#     scales <- ggproto_parent(FacetWrap, self)$init_scales(layout, x_scale, y_scale, params)
+#     
+#     if(is.null(params$scale_overrides)) return(scales)
+#     
+#     max_scale_x <- length(scales$x)
+#     max_scale_y <- length(scales$y)
+#     
+#     # ... do some modification of the scales$x and scales$y here based on params$scale_overrides
+#     for(scale_override in params$scale_overrides) {
+#       which <- scale_override$which
+#       scale <- scale_override$scale
+#       
+#       if("x" %in% scale$aesthetics) {
+#         if(!is.null(scales$x)) {
+#           if(which < 0 || which > max_scale_x) stop("Invalid index of x scale: ", which)
+#           scales$x[[which]] <- scale$clone()
+#         }
+#       } else if("y" %in% scale$aesthetics) {
+#         if(!is.null(scales$y)) {
+#           if(which < 0 || which > max_scale_y) stop("Invalid index of y scale: ", which)
+#           scales$y[[which]] <- scale$clone()
+#         }
+#       } else {
+#         stop("Invalid scale")
+#       }
+#     }
+#     
+#     # return scales
+#     scales
+#   }
+# )
+# 
+# facet_wrap_custom <- function(..., scale_overrides = NULL) {
+#   # take advantage of the sanitizing that happens in facet_wrap
+#   facet_super <- facet_wrap(...)
+#   
+#   # sanitize scale overrides
+#   if(inherits(scale_overrides, "scale_override")) {
+#     scale_overrides <- list(scale_overrides)
+#   } else if(!is.list(scale_overrides) || 
+#             !all(vapply(scale_overrides, inherits, "scale_override", FUN.VALUE = logical(1)))) {
+#     stop("scale_overrides must be a scale_override object or a list of scale_override objects")
+#   }
+#   
+#   facet_super$params$scale_overrides <- scale_overrides
+#   
+#   ggproto(NULL, CustomFacetWrap,
+#           shrink = facet_super$shrink,
+#           params = facet_super$params
+#   )
+# }
 
 
 #### OLD CODE ####
